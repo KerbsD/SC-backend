6e59eedee1fbe7698b00b54812bc917a78261e4b
KerbsD <boijablo555@gmail.com>
2025-02-13 09:35:08 +0800

    Initial commit

---
+++ controllers/authController.js
@@ -0,0 +1,48 @@
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const handleLogin = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+
+    const foundUser = await User.findOne({ username: user }).exec();
+    if (!foundUser) return res.status(401).json({"message":"User not found."}); //Unauthorized 
+    // evaluate password 
+    const match = await bcrypt.compare(pwd, foundUser.password);
+    if (match) {
+        const roles = Object.values(foundUser.roles).filter(Boolean);
+        // create JWTs
+        const accessToken = jwt.sign(
+            {
+                "UserInfo": {
+                    "username": foundUser.username,
+                    "roles": roles
+                }
+            },  
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '15s' }
+        );
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+        // Saving refreshToken with current user
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result);
+        console.log(roles);
+
+        // Creates Secure Cookie with refresh token
+        res.cookie('jwt', refreshToken, { httpOnly: true,  maxAge: 24 * 60 * 60 * 1000 });
+        // secure: true, sameSite: 'None',
+        // Send authorization roles and access token to user
+        res.json({  roles, accessToken  });
+
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = { handleLogin };
